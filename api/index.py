3157f6a3bda10d3438b42c4d15d0748f02a79433
The-Coder-Kishor <58686913+The-Coder-Kishor@users.noreply.github.com>
2023-04-20 12:34:08 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -16,96 +16,6 @@ def score_app():
     if app_no in dets:
         return jsonify(dets[app_no])
 
-@app.route('/score_link', methods=['POST'])
-@cross_origin()
-def calculate_score_link():
-    app_no = request.form['app_no']
-    answer_key = request.form['ans_link']        
-    response_sheet = request.form['res_link']
-    #Answer_Key
-    soup = BeautifulSoup(answer_key, "html.parser")
-    table = soup.find("table", {"id": "ctl00_LoginContent_grAnswerKey"})
-    key = {}
-    bonus = []
-    counter = 102
-    question_2 = "_lbl_QuestionNo"
-    answer_2 = "_lbl_RAnswer"
-    status_2 = "lblstatus"
-    ids = soup.find_all('span')
-    question = []
-    answer = []
-    for val in ids:
-        if val.get('id') != None:
-            if question_2 in val.get('id'):
-                question.append(val.text)
-            if answer_2 in val.get('id'):
-                answer.append(val.text)
-            if status_2 in val.get('id'):
-                bonus.append(question[-1])
-                del question[-1] 
-    print(len(question))
-    for i in range(0,len(question)):
-        key[question[i]] = answer[i]
-
-    #Response_Sheet
-    soup = BeautifulSoup(response_sheet, "html.parser")
-    tables = soup.find_all("table", {"class": "menu-tbl"})
-    values = {}
-    data = []
-    for table in tables:
-        sub = []
-        for row in table.find_all("tr"):
-            cells = row.find_all("td")
-            if len(cells) > 0:
-                sub.extend([cell.text for cell in cells])
-        data.append(sub)
-    answers = []
-    tables = soup.find_all("table",{"class": "questionRowTbl"})
-    for table in tables:
-        sub = []
-        for row in table.find_all("tr"):
-            cells = row.find_all("td")
-            if len(cells) > 0:
-                sub.extend([cell.text for cell in cells])
-        answers.append(sub)
-    counter = 0
-    for question in data:
-        if 'MCQ' in question and ((('Answered' in question) or ('Marked For Review' in question)) and (not ('Not Attempted and Marked For Review' in question))):
-            chosen = int(question[15])
-            question_id = question[3]
-            answer_id = question[3+chosen*2]
-            values[question_id] = answer_id
-        if 'SA' in question:
-            answer = answers[counter]
-            if ((('Answered' in question) or ('Marked For Review' in question)) and (not ('Not Attempted and Marked For Review' in question))):
-                question_id = question[3]
-                answer_id = answer[5]
-                values[question_id] = answer_id
-        counter += 1
-
-    #Calculate Score
-    correct = 0
-    incorrect = 0
-    incorr_q = []
-    corr_q = []
-    for elem in values:
-        if(elem in bonus):
-            continue
-        if(key[elem] == values[elem]):
-            corr_q.append(elem)
-            correct += 1
-        else:
-            incorrect += 1
-            incorr_q.append(elem)
-    bonus = len(bonus)
-    total = correct*4 - incorrect
-    
-    result = {'Correct': correct, 'Incorrect': incorrect, 'Total': total,
-              'Incorrect_Questions': incorr_q, 'Bonus': bonus}
-    dets[app_no] = result
-    return jsonify(result)
-
-
 @app.route('/score_file', methods=['POST'])
 @cross_origin()
 def calculate_score_file():
@@ -181,12 +91,20 @@ def calculate_score_file():
     for elem in values:
         if(elem in bonus):
             continue
-        if(key[elem] == values[elem]):
-            corr_q.append(elem)
-            correct += 1
+        if (values[elem] is not float):
+            if(key[elem] == values[elem]):
+                corr_q.append(elem)
+                correct += 1
+            else:
+                incorrect += 1
+                incorr_q.append(elem)
         else:
-            incorrect += 1
-            incorr_q.append(elem)
+            if(float(key[elem] == values[elem])):
+                corr_q.append(elem)
+                correct += 1
+            else:
+                incorrect += 1
+                incorr_q.append(elem)
     bonus = len(bonus)
     total = correct*4 - incorrect
     
